84a38485e1f2494eebb69f1e69395a7cbf7b5565
가입dldidsla13 <dldidsla13@naver.com>
2023-11-21 09:24:02 +0900

    CHEN-24 chatting 끝

--- src/main/java/com/kdt/dto/ChatMessageDTO.java
+++ src/main/java/com/kdt/dto/ChatMessageDTO.java
@@ -5,6 +5,7 @@ import java.sql.Timestamp;
 public class ChatMessageDTO {
 	private int MessageSeq;
 	private int roomID; 
+	private String profile_image; 
 	private String userID;
     private String message;
     private String messageType;
@@ -12,74 +13,99 @@ public class ChatMessageDTO {
     private Timestamp write_date;
     
     public ChatMessageDTO() {}
-    
-	public ChatMessageDTO(int messageSeq, int roomID, String userID, String message, String messageType,
-			MessageType type, Timestamp write_date) {
+	public ChatMessageDTO(int messageSeq, int roomID, String profile_image, String userID, String message,
+			String messageType, MessageType type, Timestamp write_date) {
 		super();
 		MessageSeq = messageSeq;
 		this.roomID = roomID;
+		this.profile_image = profile_image;
 		this.userID = userID;
 		this.message = message;
 		this.messageType = messageType;
 		this.type = type;
 		this.write_date = write_date;
 	}
+
 
 	public int getMessageSeq() {
 		return MessageSeq;
 	}
 
+
 	public void setMessageSeq(int messageSeq) {
 		MessageSeq = messageSeq;
 	}
+
 
 	public int getRoomID() {
 		return roomID;
 	}
 
+
 	public void setRoomID(int roomID) {
 		this.roomID = roomID;
 	}
+
+
+	public String getProfile_image() {
+		return profile_image;
+	}
+
+
+	public void setProfile_image(String profile_image) {
+		this.profile_image = profile_image;
+	}
+
 
 	public String getUserID() {
 		return userID;
 	}
 
+
 	public void setUserID(String userID) {
 		this.userID = userID;
 	}
+
 
 	public String getMessage() {
 		return message;
 	}
 
+
 	public void setMessage(String message) {
 		this.message = message;
 	}
+
 
 	public String getMessageType() {
 		return messageType;
 	}
 
+
 	public void setMessageType(String messageType) {
 		this.messageType = messageType;
 	}
+
 
 	public MessageType getType() {
 		return type;
 	}
 
+
 	public void setType(MessageType type) {
 		this.type = type;
 	}
 
-	public Timestamp getwrite_date() {
+
+	public Timestamp getWrite_date() {
 		return write_date;
 	}
 
-	public void setwrite_date(Timestamp write_date) {
+
+	public void setWrite_date(Timestamp write_date) {
 		this.write_date = write_date;
 	}
+
 
 	public enum MessageType {
         CHAT, JOIN, LEAVE
